48441059f3cd8729d5b16e870b37b9ecfcd4800d
Hamza Ali <hamza.ali@codora.io>
2025-04-24 20:03:01 +0500

    feat: like submission , created a resetstate function to comply with DRY prenciple

--- src/App.js
+++ src/App.js
@@ -1,41 +1,54 @@
-import React, { useEffect, useState } from 'react';
-import Container from '@mui/material/Container';
-// import { Snackbar } from '@mui/material';
-import Header from './Header';
-import Content from './Content';
-import { onMessage } from './service/mockServer';
-import Snackbar from './components/snackbar';
-import toast, { Toaster } from 'react-hot-toast';
-
+import React, { useEffect, useState } from "react";
+import Container from "@mui/material/Container";
+import Header from "./Header";
+import Content from "./Content";
+import { onMessage, saveLikedFormSubmission } from "./service/mockServer";
+import Snackbar from "./components/snackbar";
+import toast, { Toaster } from "react-hot-toast";
 
 function App() {
-  const [isShowSnackbar,setIsShowSnackbar] = useState(false)
-  const [formData,setFormData] = useState(null)
+  const [isShowSnackbar, setIsShowSnackbar] = useState(false);
+  const [formData, setFormData] = useState(null);
 
-  const handleNewFormSubmission = async(data) =>{
-    setIsShowSnackbar(true)
-    setFormData(data)
+  const handleNewFormSubmission = async (data) => {
+    setIsShowSnackbar(true);
+    setFormData(data);
+  };
+
+  const resetStates = () =>{
+    setIsShowSnackbar(false)
+    setFormData(null)
   }
 
-  useEffect(()=>{
-    try{
-      onMessage(handleNewFormSubmission)
-    }catch(error){
-      console.log("Error",error)
-      toast.error("Something went wrong while setting up the callback")
-
+  const handleSubmissionLike = async (data) => {
+    try {
+      await saveLikedFormSubmission(data);
+      resetStates()
+      toast.success("You have liked the submission");
+    } catch (error) {
+      console.log("Error", error);
+      toast.error("Unable to Like the submission");
     }
-  },[])
+  };
+
+  useEffect(() => {
+    try {
+      onMessage(handleNewFormSubmission);
+    } catch (error) {
+      console.log("Error", error);
+      toast.error("Something went wrong while setting up the callback");
+    }
+  }, []);
 
   return (
     <>
-    <Snackbar open={isShowSnackbar} onClose={()=>{
-      setIsShowSnackbar(false)
-      setFormData(null)
-    }} 
-    content={formData}
-    onLike={()=>console.log("Like button click")}/>
-    <Toaster position="bottom-center" reverseOrder={false} />
+      <Snackbar
+        open={isShowSnackbar}
+        onClose={resetStates}
+        content={formData}
+        onLike={handleSubmissionLike}
+      />
+      <Toaster position="bottom-center" reverseOrder={false} />
       <Header />
       <Container>
         <Content />
