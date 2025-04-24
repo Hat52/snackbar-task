5bf63c65abfa60871d0cea83b7d124e46b02f0fe
Hamza Ali <hamza.ali@codora.io>
2025-04-24 20:25:41 +0500

    feat: list all likedSubmissions

--- src/Content.js
+++ src/Content.js
@@ -1,15 +1,33 @@
-import React from 'react';
-import Box from '@mui/material/Box';
-import Typography from '@mui/material/Typography';
+import React from "react";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import { Card, CardContent } from "@mui/material";
 
-export default function Content() {
+export default function Content({ likedSubmissions }) {
   return (
-    <Box sx={{marginTop: 3}}>
+    <Box sx={{ marginTop: 3 }}>
       <Typography variant="h4">Liked Form Submissions</Typography>
 
-      <Typography variant="body1" sx={{fontStyle: 'italic', marginTop: 1}}>
-        TODO: List of liked submissions here (delete this line)
-      </Typography>
+      {likedSubmissions?.length ? (
+        <Box display="flex" flexDirection="column" gap={3} my={3}>
+          {likedSubmissions?.map(({ data, id }) => (
+            <Card key={id}>
+              <CardContent sx={{ height: "100%" }}>
+                <Typography variant="h5" component="div">
+                  {`${data.firstName ?? "-"} ${data.lastName ?? "-"}`}
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                  {data.email}
+                </Typography>
+              </CardContent>
+            </Card>
+          ))}
+        </Box>
+      ) : (
+        <Typography variant="body1" sx={{ fontStyle: "italic", marginTop: 1 }}>
+          No Liked Submission Found
+        </Typography>
+      )}
     </Box>
   );
 }
