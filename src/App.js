64e1695133284ddee4e82f275245603f2fca6aea
Hamza Ali <hamza.ali@codora.io>
2025-04-24 19:59:29 +0500

    feat: show toast on successful form submission, new package added for showing error toast

--- src/App.js
+++ src/App.js
@@ -1,22 +1,41 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import Container from '@mui/material/Container';
-
+// import { Snackbar } from '@mui/material';
 import Header from './Header';
 import Content from './Content';
 import { onMessage } from './service/mockServer';
+import Snackbar from './components/snackbar';
+import toast, { Toaster } from 'react-hot-toast';
+
 
 function App() {
+  const [isShowSnackbar,setIsShowSnackbar] = useState(false)
+  const [formData,setFormData] = useState(null)
 
-  const handleNewFormSubmission = (data) =>{
-    console.log("Callback has been called", data)
+  const handleNewFormSubmission = async(data) =>{
+    setIsShowSnackbar(true)
+    setFormData(data)
   }
 
   useEffect(()=>{
-    onMessage(handleNewFormSubmission)
+    try{
+      onMessage(handleNewFormSubmission)
+    }catch(error){
+      console.log("Error",error)
+      toast.error("Something went wrong while setting up the callback")
+
+    }
   },[])
 
   return (
     <>
+    <Snackbar open={isShowSnackbar} onClose={()=>{
+      setIsShowSnackbar(false)
+      setFormData(null)
+    }} 
+    content={formData}
+    onLike={()=>console.log("Like button click")}/>
+    <Toaster position="bottom-center" reverseOrder={false} />
       <Header />
       <Container>
         <Content />
