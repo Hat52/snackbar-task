5a71f8f49e4e10fbeca46ae8dff8246417f139bb
Hamza Ali <hamza.ali@codora.io>
2025-04-24 17:31:35 +0500

    feat: register callback

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Container from '@mui/material/Container';
 
 import Header from './Header';
 import Content from './Content';
+import { onMessage } from './service/mockServer';
 
 function App() {
+
+  const handleNewFormSubmission = (data) =>{
+    console.log("Callback has been called", data)
+  }
+
+  useEffect(()=>{
+    onMessage(handleNewFormSubmission)
+  },[])
+
   return (
     <>
       <Header />
